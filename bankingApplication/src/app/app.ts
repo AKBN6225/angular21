cee041b894f5514b2f2cd5955b1682d0468ca8c4
arun <arunkumar.bantupalli@gmail.com>
2026-05-06 22:21:23 +0530

    header and footer configurations

--- bankingApplication/src/app/app.ts
+++ bankingApplication/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './topics/components/header/header'
+import { Footer } from './topics/components/footer/footer'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
