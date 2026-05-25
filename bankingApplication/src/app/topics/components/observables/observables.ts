81c466ab53023fe74fa07237692663de270ffcb9
arun <arunkumar.bantupalli@gmail.com>
2026-05-25 22:28:13 +0530

    promises and observables

--- bankingApplication/src/app/topics/components/observables/observables.ts
+++ bankingApplication/src/app/topics/components/observables/observables.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, lastValueFrom  } from 'rxjs';
 
 @Component({
   selector: 'app-observables',
@@ -6,4 +7,122 @@ import { Component } from '@angular/core';
   templateUrl: './observables.html',
   styleUrl: './observables.scss',
 })
-export class Observables {}
+export class Observables implements OnInit {
+  observablesConcepts = ["1.observable definition","2.promises are eager & observables are lazy", "3.creating observable with subscribing & unsubscribing","4.diff btw observable and subject"];
+  subjectsConcepts = ["1.Subject", "2.BehaviouralSubject", "3.ReplaySubject", "4.AsyncSubject"];
+  rxjsOperators = ["one", "two", "three"];
+
+
+  constructor() {
+
+  };
+
+  ngOnInit() {
+    this.observableDefinition1();
+    this.checkingEager2();
+    this.creatingObservableSubcribingUnsubscribing3();
+    this.diffBtwObservableAndSubject4();
+  };
+
+  observableDefinition1(){
+    // an observable is an object that represents a stream of asynchronus values.
+    // it does not hold one eventual result like promise, instead it can emit multiple values over time and you can react to each emission by subscribing
+  };
+
+  checkingEager2() {
+    let mypromise = new Promise((resolve, reject) => {
+      console.log("promise is eager");
+    });
+
+    let myObservable = new Observable((observer) => {
+      console.log("observable is lazy");
+    });
+    // here the promise log is printed , which means the inner code is executed even if we are not consuming
+    // the observable log is not printed untill unless we subscribe
+
+    myObservable.subscribe((data) => {
+      console.log(data);
+    })
+    // now here the log is created which means when we subscribe only the constructor function of observable get's executed
+  };
+
+  async creatingObservableSubcribingUnsubscribing3() {
+
+    let myObservable = new Observable((observer) => {
+      let count = 0;
+
+      const intervalId = setInterval(() => {
+        if(count < 5){
+          count++;
+          observer.next(count);
+        }else{
+          // observer.error("limit reached");  // either or complete can be called once
+          observer.complete();
+          clearInterval(intervalId);
+        }
+      },1000)
+
+    });
+
+    myObservable.subscribe((data) => {
+      console.log(data);
+    },(err) => {
+      console.log(err);
+    }, () => {
+      console.log("execution completed");
+    });
+
+    /*
+    unsubscribe we can not directly do on observabe , we need to assign observable to a reference and through reference we need to do un subscribe
+    */
+
+    const result = await lastValueFrom(myObservable);
+
+    // await is only for promises, so the above line is converting observable to promise with pre defined method lastValueFrom
+
+    let obsRef = myObservable.subscribe((data) => {
+      console.log("second time calling",data);
+      if(data == 3){
+        obsRef.unsubscribe();
+      }
+    },(err) => {
+      console.log(err);
+    }, () => {
+      console.log("execution completed");
+    });
+
+    // Below is new syntax in object and keywords
+
+    // myObservable.subscribe({next :(data) => {
+    //   console.log(data);
+    // },error:(err) => {
+    //   console.log(err);
+    // }, complete: () => {
+    //   console.log("execution completed");
+    // }});
+
+    // once error is called we can not call and print complete ; 
+  };
+
+  diffBtwObservableAndSubject4(){
+    // Observable → values are produced inside the constructor function you pass when creating it. The producer logic runs only when someone subscribes, and then it emits values to that subscriber.
+    // Subject → you don’t define a producer function. Instead, you manually push values into it using .next(), and it immediately emits those values to all active subscribers.
+
+    // observables are unicast : which means we can consume in same component
+    // subjects are multicast : which means we can consume and we can create manually and we can send data to other components
+
+    // wrt signals
+    // signals can pass data for sync operations and with one dynamic value
+    // subjects can handle bith sync and async operations and can pass multiple values
+
+    // signals can no handle like switchmap, debounce and continous stream of api data like that
+
+  };
+
+  naviagteToRes(val: any){
+    console.log(val);
+    
+  }
+
+
+}
--- bankingApplication/src/app/topics/components/promises/promises.ts
+++ bankingApplication/src/app/topics/components/promises/promises.ts
@@ -6,17 +6,276 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './promises.html',
   styleUrl: './promises.scss',
 })
-export class Promises implements OnInit{
+export class Promises implements OnInit {
 
-  promisesTopics = ["async,await", ".then", "promise.all","promise.allSetteld", "promise.race", "promise.async"]
+  promisesTopics = ["1.calling api through fetch", "2.creating two promises and checing caching", "3.promise chaning", "4.promise api's(promise.all(),promise.allSetteled(),promise.race(),promise.any())", "5.async,await"]
 
 
-  constructor(){
+  constructor() {
 
   };
 
   ngOnInit() {
-    
+    this.urlCalling1();
+    this.creating2promises();
+    this.creatingPromiseChaining();
+    this.promiseApis();
+    this.asyncAwait();
   };
-  
+
+  async urlCalling1() {
+    let url = 'https://dummyjson.com/users';
+
+    let urlResult = fetch(url).then((x) => {
+      return x.json();
+    }).then((y) => {
+      return y;
+    });
+    console.log(await urlResult);
+  };
+
+  creating2promises() {
+
+    let p1 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let flag = true;
+        if (flag) {
+          resolve("p1 resolved");
+        } else {
+          reject("p1 rejected");
+        }
+      }, 3000);
+    });
+
+    let p2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let flag = false;
+        if (flag) {
+          resolve("p2 resolved");
+        } else {
+          reject("p2 rejected");
+        }
+      }, 5000);
+    });
+
+    p1.then((x) => {
+      console.log(x)
+    }).catch((err) => {
+      console.log(err);
+
+    });
+
+    p2.then((x) => {
+      console.log(x);
+    }).catch((err) => {
+      console.log(err);
+    })
+  };
+
+  creatingPromiseChaining() {
+
+    let f1 = function chaning1() {
+      let p1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let flag = true;
+          if (flag) {
+            resolve("p1 resolved");
+          } else {
+            reject("p1 rejected");
+          }
+        }, 3000);
+      });
+      return p1;
+    };
+
+    let f2 = function chaning2() {
+      let p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let flag = false;
+          if (flag) {
+            resolve("p2 resolved");
+          } else {
+            reject("p2 rejected");
+          }
+        }, 5000);
+      });
+      return p2;
+    };
+
+    let f3 = function chaning2() {
+      let p3 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let flag = true;
+          if (flag) {
+            resolve("p3 resolved");
+          } else {
+            reject("p3 rejected");
+          }
+        }, 5000);
+      });
+      return p3;
+    };
+
+    f1().then((x) => {
+      console.log(x);
+      return f2()
+    }).then((y) => {
+      console.log(y);
+      return f3()
+    }).then((z) => {
+      console.log(z);
+    })
+      .catch((err) => {
+        console.log(err);
+      });
+    // so promise chaining .catch at last will handle the rejection at any point and will terminate; if you want at each level you need to handle at each level and the chaning execution continous  
+  };
+
+  promiseApis() {
+
+    // 1. promise.all() : it will give result only if all promises are success
+    // 2. promise.allSetteled() : it will wait for all promises to get resolve either it's success or fail
+    // 3. promise.race() : it will give first setteled promise , either success or failed one which resolves first
+    // 4. promise.any() : it will give first success promise. if all fails , it will give aggrregate error which contains all errors of promises: we need to use "err.errors"
+
+    let p1 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let flag = false;
+        if (flag) {
+          resolve("p1 resolved");
+        } else {
+          reject("p1 rejected");
+        }
+      }, 5000)
+
+    });
+
+    let p2 = new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+        let flag = false;
+
+        if (flag) {
+          resolve("p2 resolved");
+        } else {
+          reject("p2 rejected");
+        }
+      }, 3000)
+
+    });
+
+    let p3 = new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+        let flag = false;
+
+        if (flag) {
+          resolve("p3 resolved");
+        } else {
+          reject("p3 rejected");
+        }
+      }, 6000)
+
+    });
+
+    Promise.all([p1, p2, p3]).then((x) => {
+      console.log("APIS ALL :", x);
+    }).catch((err) => {
+      console.log('APIS ALL ERR BLOCK', err);
+    });
+
+    Promise.allSettled([p1, p2, p3]).then((x) => {
+      console.log("APIS ALL SETTELED :", x);
+    }).catch((err) => {
+      console.log(err);
+    });
+
+    Promise.race([p1, p2, p3]).then((x) => {
+      console.log("APIS RACE :", x);
+    }).catch((err) => {
+      console.log(err);
+
+    });
+
+    Promise.any([p1, p2, p3]).then((x) => {
+      console.log("APIS ANY :", x)
+    }).catch((err) => {
+      console.log(err.errors);
+    });
+  };
+
+
+  async asyncAwait() {
+    // async functions always return promise
+    // functions start with async are async functions
+    // await can be used inside async functions: await is equivalent to .then()
+    // if we use await , untill the line execution completes it will not move further
+    // try, catch blocks are used inside async function to handle errors similar to .then, .catch
+
+    let p1 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let flag = true;
+        if (flag) {
+          resolve("p1 resolved");
+        } else {
+          reject("p1 rejected")
+        }
+      }, 3000)
+    });
+
+    let p2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let flag = false;
+        if (flag) {
+          resolve("p2 resolved")
+        } else {
+          reject("p2 rejected")
+        }
+      }, 4000);
+    });
+
+    try {
+      let p1Result = await p1;
+      console.log("asyn await:", p1Result);
+
+      let p2Result = await p2;
+      console.log("async await", p2Result);
+    } catch (err) {
+      console.log("async await",err);
+      
+    }
+
+    try {
+      let url = 'https://dummyjson.com/users';
+      let urlResult = await fetch(url);
+      let urlResultJson = await urlResult.json()
+      console.log("jjj",urlResultJson);
+      
+    }catch(err){
+      console.log(err);  
+    }
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
